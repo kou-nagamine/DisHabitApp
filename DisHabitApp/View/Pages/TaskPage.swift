98eaec8fc043a78f05643e602f64784d55acc2b2
nafell <pixel68.43@gmail.com>
2025-04-25 10:19:10 +0900

    Add tasks query to UI

--- DisHabitApp/View/Pages/TaskPage.swift
+++ DisHabitApp/View/Pages/TaskPage.swift
@@ -6,40 +6,43 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct TaskPage: View {
     var objective: SchemaV1.Objective
-//    @ObservedObject var vm: TasksPageViewModel
+    
+    @Query var allTasks: [SchemaV1.StandbyTask]
+    var tasks: [SchemaV1.StandbyTask] {
+        return allTasks.filter { $0.objective?.id == objective.id }
+    }
     
     init(objective: SchemaV1.Objective) {
         self.objective = objective
-//        self.vm = .init(selectedObjective: objective)
     }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-//            Text(vm.selectedObjective.text)
-//                .font(.system(size: 30, weight: .bold))
-//                .padding(.top, 20)
-//                .padding(.bottom, 35)
-//                .padding(.leading, 30)
+            Text(objective.text)
+                .font(.system(size: 30, weight: .bold))
+                .padding(.top, 20)
+                .padding(.bottom, 35)
+                .padding(.leading, 30)
             ScrollView {
                 VStack(alignment: .leading, spacing: 15) {
-//                    ForEach(vm.tasks) { task in
-//                        Text(task.text)
-//                            .font(.system(size: 18, weight: .bold))
-//                            .frame(maxWidth: .infinity)
-//                            .frame(height: 55)
-//                            .background(.white, in: RoundedRectangle(cornerRadius: 15))
-//                            .padding(.trailing, 200)
-//                            .overlay {
-//                                RoundedRectangle(cornerRadius: 15)
-//                                    .stroke(lineWidth: 2)
-//                                    .fill(.gray.gradient)
-//                                    .padding(.horizontal, 30)
-//                            }
-//
-//                    }
+                    ForEach(tasks) { task in
+                        Text(task.text)
+                            .font(.system(size: 18, weight: .bold))
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 55)
+                            .background(.white, in: RoundedRectangle(cornerRadius: 15))
+                            .padding(.trailing, 200)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(lineWidth: 2)
+                                    .fill(.gray.gradient)
+                                    .padding(.horizontal, 30)
+                            }
+                    }
                 }
             }
         }
