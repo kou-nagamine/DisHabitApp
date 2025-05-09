984ab5bf0b3f8ccc9bbe4b5ca1d4e0a808659212
nafell <pixel68.43@gmail.com>
2025-05-09 15:32:56 +0900

    AddQuestPage: バリデーション処理を作成

--- DisHabitApp/View/Pages/AddQuestPage.swift
+++ DisHabitApp/View/Pages/AddQuestPage.swift
@@ -24,28 +24,47 @@ struct AddQuestPage: View {
         for (index, value) in self.days.enumerated() {
             dict[index+1] = WeekDayList.contains(value)
         }
-        print(dict.description)
+//        print(dict.description)
         return dict
     }
     
     @State var selectedTasks: [SchemaV1.StandbyTask] = []
     
+    @State var isSubmitPressed: Bool = false
+    
     private func createQuest() {
+        isSubmitPressed = true
+        
+        if !validateAll {
+            print("validate fail")
+        }
+        
+        print("validate pass")
+        
         let newQuest = SchemaV1.Quest(activatedDayOfWeeks: self.weekDayDict, reward: SchemaV1.Reward(text: self.rewardName), tasks: self.selectedTasks)
         
         modelContext.insert(newQuest)
     }
     
-    // validatorをcomputed propertyにする
+    /// validator: true == success; false == error
     
-    // []=pass, error=some string
-    private func validate() -> [String] {
-        // name is empty
-        // occasion is 0
-        // no tasks
-        
-        // rewardpoint can be nil??
-        return []
+    /// rule: Name should not be empty.
+    var validateName: Bool {
+        return rewardName != ""
+    }
+    
+    /// rule: Quest should occur at least once per week.
+    var validateOccurence: Bool {
+        return weekDayDict.occasionCount() > 0
+    }
+    
+    /// rule: At least one task should be selected.
+    var validateSelectedTasks: Bool {
+        return selectedTasks.count > 0
+    }
+    
+    var validateAll: Bool {
+        return validateName && validateOccurence && validateSelectedTasks
     }
     
     var body: some View {
@@ -62,20 +81,38 @@ struct AddQuestPage: View {
                     }
                     .padding(.top, 30)
                     Spacer()
-                    VStack (spacing: 30){
-                        TextField("楽しいこと", text: $rewardName)
-                            .font(.largeTitle)
-                            .padding(.bottom, 10)
-                            .overlay(alignment: .bottom) {
-                                Rectangle()
-                                    .frame(height: 1)
-                                    .foregroundColor(.gray)
+                    VStack (spacing: 0){
+                        VStack(alignment: .leading) {
+                            TextField("楽しいこと", text: $rewardName)
+                                .font(.largeTitle)
+                                .padding(.bottom, 10)
+                                .overlay(alignment: .bottom) {
+                                    Rectangle()
+                                        .frame(height: 1)
+                                        .foregroundColor(.gray)
+                                }
+                            if isSubmitPressed && !validateName {
+                                Text("*楽しいことを入力してください")
+                                    .font(.callout).foregroundColor(.red)
+                                    .frame(height:22, alignment: .leading)
+                            } else {
+                                Spacer().frame(height:30)
                             }
+                        }
+
+                        
                         VStack(alignment: .leading, spacing: 5){
-                            Text("頻度: \(weekDayDict.weeklyOccasionDescription())")
-                                .font(.title2)
-                                .fontWeight(.bold)
-                                .padding(.vertical, 10)
+                            HStack {
+                                Text("頻度: \(weekDayDict.weeklyOccasionDescription())")
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                    .padding(.vertical, 10)
+                                if isSubmitPressed && !validateOccurence {
+                                    Text("*週1回以上を選択してください")
+                                        .font(.callout).foregroundColor(.red)
+                                }
+                            }
+
 
                             HStack(spacing: 7) {
                                 ForEach(days, id: \.self) { day in
@@ -132,12 +169,21 @@ struct AddQuestPage: View {
             }
             .frame(height: 400)
             VStack(alignment: .leading, spacing: 0){
-                Text("やることリスト")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.leading, 30)
-                    .padding(.top, 30)
-                    .padding(.bottom, 15)
+                HStack(alignment: .bottom) {
+                    Text("やることリスト")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.leading, 30)
+                        .padding(.top, 30)
+                        .padding(.bottom, 15)
+                    
+                    if isSubmitPressed && !validateSelectedTasks {
+                        Text("*追加してください")
+                            .font(.callout).foregroundColor(.red)
+                            .padding(.bottom, 20)
+                    }
+                }
+
                 ScrollView {
                     VStack (spacing: 20){
                         ForEach (selectedTasks) { task in
@@ -156,11 +202,11 @@ struct AddQuestPage: View {
                         createQuest()
                     }
                    ,label:{
-                    Text("登録")
+                       Text("作成")
                         .font(.headline)
                         .foregroundColor(.white)
                         .frame(width: 300, height: 50)
-                        .background(Color.blue)
+                        .background(validateAll ? Color.blue : Color.gray)
                         .cornerRadius(10)
                 })
             }
