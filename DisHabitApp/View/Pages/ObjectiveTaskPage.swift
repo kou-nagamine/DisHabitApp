037ffe02676c58f6335b7a2f7cb9ff4c40cb420e
nafell <pixel68.43@gmail.com>
2025-05-14 21:14:17 +0900

    目標/タスクの初期化ボタンをObjectiveTaskPageに追加

--- DisHabitApp/View/Pages/ObjectiveTaskPage.swift
+++ DisHabitApp/View/Pages/ObjectiveTaskPage.swift
@@ -6,6 +6,8 @@ struct ObjectiveTaskPage: View {
     @Query var objectives: [Objective]
     @Query var tasks: [StandbyTask]
     
+    @Environment(\.modelContext) private var modelContext
+    
     var body: some View {
         VStack(spacing: 10) {
             Text("タスク一覧")
@@ -13,6 +15,29 @@ struct ObjectiveTaskPage: View {
                 .padding(.vertical, 30)
             ScrollView {
                 VStack(alignment: .leading, spacing: 20) {
+                    if tasks.isEmpty {
+                        Button(action: {
+                            let objective1 = Objective(id: UUID(), text: "健康的な生活を送る")
+                            let objective2 = Objective(id: UUID(), text: "勉強習慣を身につける")
+                            let objective3 = Objective(id: UUID(), text: "運動を習慣化する")
+
+                            let task1 = StandbyTask(id: UUID(), text: "朝7時に起床する", objective: objective1)
+                            let task2 = StandbyTask(id: UUID(), text: "朝食を食べる", objective: objective1)
+                            let task3 = StandbyTask(id: UUID(), text: "1時間勉強する", objective: objective2)
+                            let task4 = StandbyTask(id: UUID(), text: "30分ジョギングする", objective: objective3)
+                            let task5 = StandbyTask(id: UUID(), text: "ストレッチをする", objective: objective3)
+                            
+                            modelContext.insert(objective1)
+                            modelContext.insert(objective2)
+                            modelContext.insert(objective3)
+                            modelContext.insert(task1)
+                            modelContext.insert(task2)
+                            modelContext.insert(task3)
+                            modelContext.insert(task4)
+                            modelContext.insert(task5)
+                            
+                        }, label: { Text("目標/タスク初期化") })
+                    }
                     ForEach(objectives) { objective in
                         Text(objective.text)
                             .font(.title2)
