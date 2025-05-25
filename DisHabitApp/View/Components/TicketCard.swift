f689d724c44e134a0310188bed6af5f31918fd54
長峯 幸佑 <k.nagamine@nagaminenoMacBook-Air.local>
2025-05-26 01:12:31 +0900

    add ticket style

--- DisHabitApp/View/Components/TicketCard.swift
+++ DisHabitApp/View/Components/TicketCard.swift
@@ -26,43 +26,31 @@ struct TicketCard: View {
             .background(/*Color.cyan.opacity(0.3)*/ .white)
             ///  左のみが丸みを帯びるように切り取ってる
             .clipShape(
-                .rect(
-                    topLeadingRadius: 5,
-                    bottomLeadingRadius: 5,
-                    bottomTrailingRadius: 0,
-                    topTrailingRadius: 0
-                )
+                TicketTitleArea(notchRadius: 10, cornerRadius: 0)
             )
+            .compositingGroup()
+            .shadow(radius: 4, x: 2, y: 2)
             /// Ticket Tear Off Area
-            HStack(spacing: 0) {
-                VStack(spacing: 5) {
-                    if manager.questSlot.acceptedQuest?.reward.isRedeemed ?? false {
-                        Text("使用済み")
-                            .font(.callout)
-                            .foregroundColor(.black.opacity(0.3))
-                        Text(manager.questSlot.acceptedQuest?.reward.redeemedDate?.formatHourTime() ?? "")
-                            .font(.title3)
-                            .foregroundColor(.black.opacity(0.3))
-                    } else {
-                        Text("期日")
-                            .font(.callout)
-                        Text(manager.questSlot.board?.date.formatMonthDay() ?? "")
-                            .font(.title3)
-                    }
-
+            VStack(spacing: 5) {
+                if manager.questSlot.acceptedQuest?.reward.isRedeemed ?? false {
+                    Text("使用済み")
+                        .font(.callout)
+                        .foregroundColor(.black.opacity(0.3))
+                    Text(manager.questSlot.acceptedQuest?.reward.redeemedDate?.formatHourTime() ?? "")
+                        .font(.title3)
+                        .foregroundColor(.black.opacity(0.3))
+                } else {
+                    Text("期日")
+                        .font(.callout)
+                    Text(manager.questSlot.board?.date.formatMonthDay() ?? "")
+                        .font(.title3)
                 }
+
             }
             .frame(width: 100, height: 110) /// 高さを指定
-            .background(manager.questSlot.acceptedQuest?.reward.isRedeemed ?? false ? .white : .cyan.opacity(0.2))
-            ///  右のみが丸みを帯びるように切り取ってる
-            .clipShape(
-                .rect(
-                    topLeadingRadius: 0,
-                    bottomLeadingRadius: 0,
-                    bottomTrailingRadius: 5,
-                    topTrailingRadius: 5
-                )
-            )
+            .background(manager.questSlot.acceptedQuest?.reward.isRedeemed ?? false ? .white : .cyan)
+            .compositingGroup()
+            .shadow(radius: 1, x: 3, y: 3)
             /// 切り取り線
             .overlay(alignment: .leading) {
                 Rectangle()
@@ -72,6 +60,9 @@ struct TicketCard: View {
                     .foregroundColor(.gray)
                     .frame(width: 2)/// 横幅をlineWidthと同じにして線にする
             }
+            .clipShape(
+                TicketTearOffArea(notchRadius: 10, cornerRadius: 0)
+            )
             .onTapGesture {
                 if manager.tense != .today {
                     return
@@ -84,18 +75,6 @@ struct TicketCard: View {
                     acceptedQuest.redeemReward()
                 }
             }
-        }
-        /// card 全体のstroke
-        .overlay() {
-            RoundedRectangle(cornerRadius: 5)
-                .stroke(
-                    .linearGradient(colors: [
-                        .cyan.opacity(0.5),
-                        .cyan.opacity(0.6),
-                        .cyan.opacity(0.7),
-                        .cyan.opacity(0.8)
-                    ], startPoint: .topLeading, endPoint: .bottomTrailing)
-                )
         }
         .frame(maxWidth: .infinity)
         .padding(.horizontal, 25)
